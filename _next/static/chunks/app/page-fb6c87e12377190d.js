95ba0f02945c5fc0dbf4edddc82345ddb1c5e3c7
rcoderx <104567101+rcoderx@users.noreply.github.com>
2024-01-14 10:58:39 -0600

    Add files via upload

---
+++ _next/static/chunks/app/page-fb6c87e12377190d.js
@@ -0,0 +1,159 @@
+(self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([
+  [931],
+  {
+    5113: function (e, t, s) {
+      Promise.resolve().then(s.bind(s, 2241));
+    },
+    2241: function (e, t, s) {
+      "use strict";
+      s.r(t),
+        s.d(t, {
+          default: function () {
+            return r;
+          },
+        });
+      var l = s(7437),
+        i = s(6691),
+        n = s.n(i),
+        a = s(2265);
+      function r() {
+        let [e, t] = (0, a.useState)(!1);
+        return (
+          (0, a.useEffect)(() => {
+            let e = navigator.userAgent || navigator.vendor || window.opera;
+            t(/iPhone|iPad|iPod|Android/i.test(e));
+          }, []),
+          (0, l.jsx)("main", {
+            className:
+              "flex items-center justify-center min-h-screen bg-white text-black p-10",
+            children: e
+              ? (0, l.jsxs)("div", {
+                  className: "text-center",
+                  children: [
+                    (0, l.jsx)("div", {
+                      className: "flex justify-center items-center",
+                      children: (0, l.jsx)(n(), {
+                        src: "/images/poot.jpg",
+                        alt: "SOAP",
+                        width: 300,
+                        height: 300,
+                      }),
+                    }),
+                    (0, l.jsx)("h1", {
+                      className: "text-4xl font-bold",
+                      children: "$SOAP",
+                    }),
+                    (0, l.jsx)("p", { children: "He tried some shit." }),
+                    (0, l.jsx)("p", { children: "He went to Prison." }),
+                    (0, l.jsx)("p", { children: "Now we are a shitcoin..." }),
+                    (0, l.jsx)("p", {
+                      children:
+                        "CA : EkoErpZPMEKnEbbi7y8Q87vZ59EwmiW7YK4qdvbkat3p",
+                    }),
+                    (0, l.jsx)("a", {
+                      href: "https://t.me/PootSol",
+                      className: "block mt-4",
+                      children: (0, l.jsx)("button", {
+                        className:
+                          "bg-black text-white font-bold py-2 px-4 rounded",
+                        children: "Telegram",
+                      }),
+                    }),
+                    (0, l.jsx)("a", {
+                      href: "https://twitter.com/PootSol",
+                      className: "block mt-4",
+                      children: (0, l.jsx)("button", {
+                        className:
+                          "bg-black text-white font-bold py-2 px-4 rounded",
+                        children: "Twitter",
+                      }),
+                    }),
+                  ],
+                })
+              : (0, l.jsxs)("div", {
+                  className: "flex flex-row items-center justify-center gap-10",
+                  children: [
+                    (0, l.jsx)("div", {
+                      children: (0, l.jsx)(n(), {
+                        src: "/images/poot.jpg",
+                        alt: "SOAP",
+                        width: 500,
+                        height: 500,
+                      }),
+                    }),
+                    (0, l.jsxs)("div", {
+                      className: "flex flex-col gap-4 items-center",
+                      children: [
+                        (0, l.jsx)("h1", {
+                          className: "text-6xl font-bold",
+                          children: "$SOAP",
+                        }),
+                        (0, l.jsx)("p", {
+                          className: "text-xl",
+                          children: "He tried some shit.",
+                        }),
+                        (0, l.jsx)("p", {
+                          className: "text-xl",
+                          children: "He went to Prison.",
+                        }),
+                        (0, l.jsx)("p", {
+                          className: "text-xl",
+                          children: "Now we are a shitcoin...",
+                        }),
+                        (0, l.jsx)("p", {
+                          className: "text-xl",
+                          children:
+                            "CA: My CA Here",
+                        }),
+                        (0, l.jsx)("a", {
+                          href: "https://raydium.io/swap/?inputCurrency=sol&outputCurrency=EkoErpZPMEKnEbbi7y8Q87vZ59EwmiW7YK4qdvbkat3p&fixed=in",
+                          className: "w-full",
+                          children: (0, l.jsx)("button", {
+                            className:
+                              "bg-black text-white p-3 rounded-lg w-full",
+                            children: "Buy Now",
+                          }),
+                        }),
+                        (0, l.jsx)("a", {
+                          href: "https://dexscreener.com/solana/EkoErpZPMEKnEbbi7y8Q87vZ59EwmiW7YK4qdvbkat3p",
+                          className: "w-full",
+                          children: (0, l.jsx)("button", {
+                            className:
+                              "bg-black text-white p-3 rounded-lg w-full",
+                            children: "Chart",
+                          }),
+                        }),
+                        (0, l.jsx)("a", {
+                          href: "https://t.me/PootSol",
+                          className: "w-full",
+                          children: (0, l.jsx)("button", {
+                            className:
+                              "bg-black text-white p-3 rounded-lg w-full",
+                            children: "Telegram",
+                          }),
+                        }),
+                        (0, l.jsx)("a", {
+                          href: "https://twitter.com/PootSol",
+                          className: "w-full",
+                          children: (0, l.jsx)("button", {
+                            className:
+                              "bg-black text-white p-3 rounded-lg w-full",
+                            children: "Twitter",
+                          }),
+                        }),
+                      ],
+                    }),
+                  ],
+                }),
+          })
+        );
+      }
+    },
+  },
+  function (e) {
+    e.O(0, [986, 971, 938, 744], function () {
+      return e((e.s = 5113));
+    }),
+      (_N_E = e.O());
+  },
+]);
